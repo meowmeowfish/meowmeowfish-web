79942681d923a0eb2ef53e78590a8a52934692df
Kamigami55 <samkami@icloud.com>
2018-02-03 18:07:48 +0800

    Update marker info-window content

--- app/controllers/gmaps_controller.rb
+++ app/controllers/gmaps_controller.rb
@@ -8,8 +8,8 @@ class GmapsController < ApplicationController
      @hash = Gmaps4rails.build_markers(@gmaps) do |gmap, marker|
        marker.lat gmap.latitude
        marker.lng gmap.longitude
-       marker.infowindow gmap.address
-    end
+       marker.infowindow "ID: #{gmap.index}<br/>Lat.: #{gmap.latitude}<br/>Lon.: #{gmap.longitude}"
+     end
   end
 
   # GET /gmaps/1
