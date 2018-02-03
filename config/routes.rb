3f91f8fb2ae28689a1734287f1e62201d8c9a5cd
Kamigami55 <samkami@icloud.com>
2018-02-03 16:58:13 +0800

    Update api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'gmaps#index'
   get 'gmaps/api/create/:index/:lat/:lon' => 'gmaps#api_create'
+  get 'gmaps/api/create' => 'gmaps#api_create'
   get 'gmaps/api/update/:index/:lat/:lon' => 'gmaps#api_update'
+  get 'gmaps/api/update' => 'gmaps#api_update'
   resources :gmaps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
